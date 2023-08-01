50830ab40708eb7e68c56604dcbf48cf3d00d713
Punsara Kasthurisinghe <kasthurisinghekdpk@gmail.com>
2023-08-01 11:56:45 +0530

    Create customer mange UI

--- src/pos/mvc/PosMvc.java
+++ src/pos/mvc/PosMvc.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pos.mvc;
+import pos.mvc.view.CustomerView;
 
 /**
  *
@@ -14,7 +15,7 @@ public class PosMvc {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello");
+        new CustomerView().setVisible(true);
     }
     
 }
